a0cd459507fd6203efc789f2ce301df6bb871989
GreedTP <alexanderwenger@outlook.com>
2024-03-03 20:39:15 +0100

    feat: add animation and swich to topbar

--- js/app.js
+++ js/app.js
@@ -1,8 +1,7 @@
 gsap.from("#greeting", {duration: 2, scale: 0.1, ease: "elastic.out(1, 0.3)"});
 gsap.to("#greeting", {duration: 2, y: "-=20", repeat: -1, yoyo: true, ease: "power1.inOut", delay: 2});
 
-document.addEventListener('DOMContentLoaded', function() {
-    document.querySelector('#toggleButton').addEventListener('click', function() {
-        document.querySelector('.sidebar').classList.toggle('expanded');
-    });
-});
+gsap.fromTo("#greeting", 
+    {scale: 0.1, rotation: 0, opacity: 0, duration: .5, ease: "power1.inOut"}, 
+    {scale: 1, rotation: 360, opacity: 1, duration: .5, ease: "power1.inOut"}
+);
