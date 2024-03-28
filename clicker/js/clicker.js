6149c98fbe6453bed8b064c7fd63442383e617ee
GreedTP <alexanderwenger@outlook.com>
2024-03-28 23:54:40 +0100

    feat: add ending and item unlock screen

--- clicker/js/clicker.js
+++ clicker/js/clicker.js
@@ -1,12 +1,13 @@
-let bananas = 0;
+let bananas = 99999999;
 
 let items = {
-  click: { name: "Click Force", description: "Generate 1 banana more per click", count: 0, cost: 50, generates: 0, lock: false, icon: "✖️" },
-  monkey: { name: "Monkey", description: "Generates 1 banana per second", count: 0, cost: 100, generates: 1, lock: false, icon: "🐒" },
-  gorilla: { name: "Gorilla", description: "Generates 8 banana per second", count: 0, cost: 870, generates: 8, lock: true, icon: "🦍" },
-  orangutan: { name: "Orangutan", description: "Generates 34 banana per second", count: 0, cost: 6800, generates: 34, lock: true, icon: "🦧" },
-  sloth: { name: "Sloth", description: "Generates 492 banana per second", count: 0, cost: 94000, generates: 492, lock: true, icon: "🦥" },
-  parrot: { name: "Parrot", description: "Generates 1356 banana per second", count: 0, cost: 7145600, generates: 1356, lock: true, icon: "🦜" },
+  0: { name: "Click Force", description: "Generate 1 banana more per click", count: 0, cost: 50, generates: 0, lock: false, icon: "✖️" },
+  1: { name: "Monkey", description: "Generates 1 banana per second", count: 0, cost: 150, generates: 1, lock: false, icon: "🐒" },
+  2: { name: "Gorilla", description: "Generates 8 banana per second", count: 0, cost: 900, generates: 9, lock: true, icon: "🦍" },
+  3: { name: "Orangutan", description: "Generates 34 banana per second", count: 0, cost: 6300, generates: 63, lock: true, icon: "🦧" },
+  4: { name: "Sloth", description: "Generates 492 banana per second", count: 0, cost: 44100, generates: 441, lock: true, icon: "🦥" },
+  5: { name: "Parrot", description: "Generates 1356 banana per second", count: 0, cost: 132300, generates: 1323, lock: true, icon: "🦜" },
+  6: { name: "Flamingo", description: "Generates 1356 banana per second", count: 0, cost: 396900, generates: 3969, lock: true, icon: "🦩" },
 };
 
 let lastClickTime = 0;
@@ -24,7 +25,7 @@ function increment() {
   }
   lastClickTime = currentTime;
 
-  bananas += 1 * (items.click.count + 1);
+  bananas += 1 * (items[0].count + 1);
   document.getElementById("counter").innerText = bananas;
   gsap.to("#clicker", { scale: 0.8, duration: 0.05, onComplete: resetScale });
   generateBanana();
@@ -40,9 +41,14 @@ function updateBananaCount() {
 }
 
 let targetBananas = bananas;
+let won = false;
 
 setInterval(() => {
   targetBananas = bananas;
+  if (won === false && bananas >= 100000000) {
+    finishGame();
+    won = true;
+  }
   for (let item in items) {
     if (items[item].count > 0) {
       targetBananas += items[item].count * items[item].generates;
@@ -87,52 +93,89 @@ let shop = document.querySelector(".shop");
 for (let item in items) {
   let shopItem = document.createElement("div");
   shopItem.className = "shop-item";
-
   let iconContainer = document.createElement("div");
   iconContainer.className = "icon-container";
-
   let icon = document.createElement("span");
-  if (items[item].lock === true) {
-    icon.textContent = "🔒";
-  }
-  else {
-    icon.textContent = items[item].icon;
-  }
-  iconContainer.appendChild(icon);
-
   let itemDetails = document.createElement("div");
   itemDetails.className = "item-details";
-
   let title = document.createElement("h3");
-  title.textContent = items[item].name
-  itemDetails.appendChild(title);
-
   let description = document.createElement("p");
-  description.textContent = items[item].description;
-  itemDetails.appendChild(description);
-
   let cost = document.createElement("button");
   cost.id = item;
   cost.addEventListener("click", () => buyItem(item));
-  cost.textContent = items[item].cost + "🍌";
-  itemDetails.appendChild(cost);
+
+
 
   if (items[item].lock === true) {
     shopItem.classList.add("shop-item-hidden");
   }
 
+  if (items[item].lock === true) {
+    icon.textContent = "🔒";
+  }
+  else {
+    icon.textContent = items[item].icon;
+    title.textContent = items[item].name
+    description.textContent = items[item].description;
+    cost.textContent = items[item].cost + "🍌";
+  }
+
+  iconContainer.appendChild(icon);
+  itemDetails.appendChild(title);
+  itemDetails.appendChild(description);
+  itemDetails.appendChild(cost);
   shopItem.appendChild(iconContainer);
   shopItem.appendChild(itemDetails);
   shop.appendChild(shopItem);
 }
 
-function unlokItem(item) {
+function unlockItem(item) {
   let button = document.getElementById(item);
   let itemDetails = button.closest('.item-details');
   let shopItem = itemDetails.closest('.shop-item');
-  shopItem.classList.remove('shop-item-hidden');
   let icon = shopItem.querySelector('.icon-container span');
+  let title = shopItem.querySelector('.item-details h3');
+  let description = shopItem.querySelector('.item-details p');
+
+  button.textContent = items[item].cost + "🍌";
+  shopItem.classList.remove('shop-item-hidden');
   icon.textContent = items[item].icon;
+  title.textContent = items[item].name;
+  description.textContent = items[item].description;
+
+  items[item].lock = false;
+}
+
+// Item Modal
+let body = document.querySelector("body");
+function unlockItemModal(item) {
+  let modal = document.createElement("div");
+  modal.className = "modal";
+  let modalContent = document.createElement("div");
+  modalContent.className = "modal-content";
+  let title = document.createElement("h2");
+  let icon = document.createElement("span");
+  let description = document.createElement("p");
+  let button = document.createElement("button");
+
+  button.addEventListener("click", () => {
+    body.removeChild(modal);
+  });
+
+  button.textContent = "Great!";
+  title.textContent = "Congratulations!";
+  description.textContent = "You unlocked: " + items[item].name + "!";
+  icon.textContent = items[item].icon;
+
+  modalContent.appendChild(title);
+  modalContent.appendChild(description);
+  modalContent.appendChild(icon);
+  modalContent.appendChild(button);
+  modal.appendChild(modalContent);
+  body.appendChild(modal);
+
+  let tl = gsap.timeline({ repeat: -1, yoyo: true });
+  tl.to(icon, { rotation: 20, duration: 0.3, ease: "power1.inOut" })
 }
 
 let keys = Object.keys(items);
@@ -153,8 +196,8 @@ function buyItem(item) {
 
     if (maxIndex > currentIndex + 1) {
       if (nextItem.lock === true) {
-        console.log(maxIndex, currentIndex + 1);
-        unlokItem(nextItemKey);
+        unlockItem(nextItemKey);
+        unlockItemModal(nextItemKey);
       }
     }
 
@@ -198,4 +241,31 @@ function generateItem(item) {
     duration: 0.2,
     ease: "power1.inOut"
   });
+}
+
+// Finish Modal
+function finishGame() {
+  let modal = document.createElement("div");
+  modal.className = "modal";
+  let modalContent = document.createElement("div");
+  modalContent.className = "modal-content";
+  let title = document.createElement("h1");
+  let icon = document.createElement("span");
+  let comment = document.createElement("p");
+  let button = document.createElement("button");
+
+  button.addEventListener("click", () => {
+    body.removeChild(modal);
+  });
+
+  button.textContent = "Continue!";
+  title.textContent = "Congratulations!";
+  comment.textContent = "Wow, 100'000'000 bananas? That's bananas! 🍌 You've officially earned the title of 'Supreme Banana Clicker Champion'—a prestigious honor only bestowed upon those brave enough to waste copious amounts of time on a fruitless waste of time. Remember, while you're basking in banana glory, the rest of us are wondering if you've gone completely banana!";
+  icon.textContent = "🎉🎉🎉";
+  modalContent.appendChild(title);
+  modalContent.appendChild(comment);
+  modalContent.appendChild(icon);
+  modalContent.appendChild(button);
+  modal.appendChild(modalContent);
+  body.appendChild(modal);
 }
