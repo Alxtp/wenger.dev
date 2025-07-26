f8a25d4c2dd2a5af2848556468df3a3596b5d15e
GreedTP <alexanderwenger@outlook.com>
2025-07-26 17:17:38 +0200

    Trash everything to make space for new site

--- js/app.js
+++
@@ -1,7 +0,0 @@
-gsap.from("#greeting", {duration: 2, scale: 0.1, ease: "elastic.out(1, 0.3)"});
-gsap.to("#greeting", {duration: 2, y: "-=20", repeat: -1, yoyo: true, ease: "power1.inOut", delay: 2});
-
-gsap.fromTo("#greeting", 
-    {scale: 0.1, rotation: 0, opacity: 0, duration: .5, ease: "power1.inOut"}, 
-    {scale: 1, rotation: 360, opacity: 1, duration: .5, ease: "power1.inOut"}
-);
